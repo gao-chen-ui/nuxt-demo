eeee70c4fc1bfcf0386ba7543b700ff5767bf8b1
gaochen <1658618328@qq.com>
2021-07-01 14:16:28 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NuxtLogo></NuxtLogo>
+    <div>
+      <form>
+        <input type="text" :class="inputStyle">
+        <button>Confirm</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    return {
+      inputStyle: 'outline-none ring-2 ring-red-400 focus:ring-blue-400 rounded-lg py-2 px-4'
+    }
+  }
+})
+</script>
